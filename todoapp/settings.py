ad1d05de50655e13a429b788b0f9d81ab97e2d1d
lijimathew123 <liji.mathew@elkanio.com>
2025-10-19 14:27:59 +0530

    change sqlite to postgresql

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL engine
+        'NAME': 'to-do-app',               # Database name
+        'USER': 'postgres',               # DB username
+        'PASSWORD': 'liji',       # DB password
+        'HOST': 'localhost',                        # or your DB server IP / domain
+        'PORT': '5432',                             # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
